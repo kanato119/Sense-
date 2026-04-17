7e4f212758c2b4a1d4356a9f529d5f0b7a2e09fa
kanato119 <kanatoro119@gmail.com>
2026-04-17 10:52:38 +0900

    Add
    Gessow関数完成

--- Assets/suzuki/Gessow.cs
+++ Assets/suzuki/Gessow.cs
@@ -2,26 +2,40 @@ using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gessow : MonoBehaviour
 {
 
    
 
-    [SerializeField] public Image Sumi;
+    [SerializeField] public GameObject Sumi;
+
+    [SerializeField] float Time;
+
+    private void Start()
+    {
+        Sumi.SetActive(false);
+    }
 
     // Start is called before the first frame update
-   
+
     private void OnCollisionEnter(Collision collision)
     {
 
-      //  Sumi.Equals = true;
+        //  Sumi.Equals = true;
 
-        Invoke("OffImag", 2.0f);
+
+
+        Sumi.SetActive(true);
+        Invoke("OffImg", 2.0f);
+
     }
 
     public void OffImg()
     {
+
+        Sumi.SetActive(false);
 
 
     } 
