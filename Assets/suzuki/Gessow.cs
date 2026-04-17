00cd383151c84092a4c8644c04adbe7563ecc536
kanato119 <kanatoro119@gmail.com>
2026-04-17 12:13:51 +0900

    修正
    ゲッソー
    BulletPrefab
    全行コメント

--- Assets/suzuki/Gessow.cs
+++ Assets/suzuki/Gessow.cs
@@ -1,43 +1,43 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+//using Microsoft.Unity.VisualStudio.Editor;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class Gessow : MonoBehaviour
-{
+//public class Gessow : MonoBehaviour
+//{
 
    
 
-    [SerializeField] public GameObject Sumi;
+//    [SerializeField] public GameObject Sumi;
 
-    [SerializeField] float Time;
+//    [SerializeField] float Time;
 
-    private void Start()
-    {
-        Sumi.SetActive(false);
-    }
+//    private void Start()
+//    {
+//        Sumi.SetActive(false);
+//    }
 
-    // Start is called before the first frame update
+//    // Start is called before the first frame update
 
-    private void OnCollisionEnter(Collision collision)
-    {
+//    private void OnCollisionEnter(Collision collision)
+//    {
 
-        //  Sumi.Equals = true;
-
-
-
-        Sumi.SetActive(true);
-        Invoke("OffImg", 2.0f);
-
-    }
-
-    public void OffImg()
-    {
-
-        Sumi.SetActive(false);
+//        //  Sumi.Equals = true;
 
 
-    } 
 
-}
+//        Sumi.SetActive(true);
+//        Invoke("OffImg", 2.0f);
+
+//    }
+
+//    public void OffImg()
+//    {
+
+//        Sumi.SetActive(false);
+
+
+//    } 
+
+//}
