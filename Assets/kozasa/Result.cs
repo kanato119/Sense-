fda6042d801d09c0def8f4c60a81048c0adf4483
etateyan <kozasa.akira0817@gmail.com>
2026-04-23 14:34:33 +0900

    リザルト修正

--- Assets/kozasa/Result.cs
+++ Assets/kozasa/Result.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Result : MonoBehaviour
 {
@@ -17,19 +20,32 @@ public class Result : MonoBehaviour
 
     void DisplayRanking()
     {
+
+
         for (int i = 0; i < 5; i++)
         {
-            float time = PlayerPrefs.GetFloat("Rank" + i, Mathf.Infinity);
+
+            int stage = GetStageNumber();
+
+            float time = PlayerPrefs.GetFloat("Stage" + stage+ "_Rank" + i, Mathf.Infinity);
 
             if (time == Mathf.Infinity)
             {
+
                 rankTexts[i].text = (i + 1) + "位 : ---";
+
             }
             else
             {
+
                 rankTexts[i].text = (i + 1) + "位 : " + time.ToString("F2") + "s";
+
             }
         }
+
+       
+
+
     }
     
 
@@ -47,9 +63,35 @@ public class Result : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
 
-            PlayerPrefs.SetFloat("Rank" + i, Mathf.Infinity);
+            int stage = GetStageNumber();
+
+            PlayerPrefs.SetFloat("Stage" + stage + "_Rank" + i, Mathf.Infinity);
 
         }
+
+    }
+
+    int GetStageNumber()
+    {
+
+        int stage = PlayerPrefs.GetInt("StageIndex", 1);
+
+        if (stage == -1)
+        {
+
+            // PlayerPrefsにない場合シーン名から取得
+            string SceneName = SceneManager.GetActiveScene().name;
+            string number;
+
+            // シーンの名前の番号を取り出す
+            number = SceneName.Replace("Stage", "");
+
+            // 数字でない場合0と表示させる
+            int.TryParse(number, out stage);
+
+        }
+
+        return stage;
 
     }
 }
