bdf4df6088e3fbea72d661800f2757468248b381
kanato119 <kanatoro119@gmail.com>
2026-05-07 14:19:21 +0900

    ADD
    SE

--- Assets/suzuki/JumpBoard.cs
+++ Assets/suzuki/JumpBoard.cs
@@ -8,12 +8,18 @@ public class JumpBoard : MonoBehaviour
     [SerializeField] private float jumpBoardFoceX = 0.0f;
     [SerializeField] private float jumpBoardFoceY = 30.0f;
     [SerializeField] private float jumpBoardFoceZ = 0.0f;
-
-
+    [SerializeField]AudioClip jumpBoardSound;
+    private AudioSource audioSource;
 
     private void OnTriggerEnter(Collider other)
     {
 
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.volume = 0.5f;
+        audioSource.PlayOneShot(jumpBoardSound);
         Rigidbody rb =  other.GetComponent<Rigidbody>();
 
 
